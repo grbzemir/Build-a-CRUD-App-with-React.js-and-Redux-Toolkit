3c5c3ad849825467fb9aca0f14d925e17333b82c
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-06 18:26:01 +0300

    .

--- Crud/src/Reducers/UserReducer.jsx
+++ Crud/src/Reducers/UserReducer.jsx
@@ -6,8 +6,19 @@ const UserSlice = createSlice({
     name: "users",
     initialState: UserList,
     reducers: {
-
+        addUser: (state, action) => {
+            console.log(action);
+            // state.push(action.payload)
+        },
+        // deleteUser: (state, action) => {
+        //     return state.filter(user => user.id !== action.payload)
+        // },
+        // updateUser: (state, action) => {
+        //     const index = state.findIndex(user => user.id === action.payload.id)
+        //     state[index] = action.payload
+        // }
     }
 })
 
+export const { addUser, deleteUser, updateUser } = UserSlice.actions
 export default UserSlice.reducer
--- Crud/src/Store/Store.jsx
+++ Crud/src/Store/Store.jsx
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit'
 import { useReducer } from 'react'
 
 
+//configureStore ile de store oluşturulur
 const store = configureStore({
     reducer: {
-
+        //store da stateler tutulur ve reducerları parametre olarak alır
         users: useReducer
 
     }
